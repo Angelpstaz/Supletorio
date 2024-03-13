7169e785a8e5ad7a2eebc0089ffaef8f8748b259
Juan <jfchanatasig1405@gmail.com>
2024-03-13 15:56:46 -0500

    Diagrama de clases

--- Pastaz.Chantasig/src/BussinesLogic/PcHormigaLarva.java
+++ Pastaz.Chantasig/src/BussinesLogic/PcHormigaLarva.java
@@ -1,8 +1,17 @@
 package BussinesLogic;
-public class PcHormigaLarva extends PcHormiga{
+public class PcHormigaLarva extends PcHormiga implements ChIHormigaLava {
     
+    public PcHormigaLarva(String chestado) {
+        super(chestado);
+    }
+
     @Override
     public String toString() {
         return getClass().getName();
+    }
+
+    @Override
+    public PcHormiga ChRealizarMetamorfosis(PcCh_Alimento alimento) {
+        throw new UnsupportedOperationException("Unimplemented method 'ChRealizarMetamorfosis'");
     }
 }
