de5e4cd13259e27043bf6cc4a112786c07b54848
Angel Pastaz <angelpstaz@gmail.com>
2024-03-13 16:16:49 -0500

    idk 3

--- Pastaz.Chantasig/src/BussinesLogic/PcHormiga.java
+++ Pastaz.Chantasig/src/BussinesLogic/PcHormiga.java
@@ -1,21 +1,21 @@
 package BussinesLogic;
 public abstract class PcHormiga {
-private String chestado="VIVA";
-private PcCh_Alimento pcCh_Alimento;
+    private String chestado="VIVA";
+    private PcCh_Alimento pcAlimento;
 
-public PcHormiga(String chestado) {
-    this.chestado = chestado;
-}
+    public PcHormiga(String chestado) {
+        this.chestado = chestado;
+    }
 
-public String getEstado() {
-    return chestado;
-}
+    public String getEstado() {
+        return chestado;
+    }
 
-public void setEstado(String chestado) {
-    this.chestado = chestado;
-}
-public PcHormiga(PcCh_Alimento pcCh_Alimento) {
-    this.pcCh_Alimento = pcCh_Alimento;
-}
+    public void setEstado(String chestado) {
+        this.chestado = chestado;
+    }
+    public PcHormiga(PcCh_Alimento pcAlimento) {
+        this.pcAlimento = pcAlimento;
+    }
  
 }
