161d372520413e6c93824ad245085fd1046e5bec
Juan <jfchanatasig1405@gmail.com>
2024-03-13 16:10:36 -0500

    Actualizacion

--- Pastaz.Chantasig/src/BussinesLogic/PcHormiga.java
+++ Pastaz.Chantasig/src/BussinesLogic/PcHormiga.java
@@ -1,11 +1,13 @@
 package BussinesLogic;
 public abstract class PcHormiga {
  private String chestado="VIVA";
-
+private PcCh_Alimento pcAlimento;
 public PcHormiga(String chestado) {
     this.chestado = chestado;
 }
-
+public PcHormiga(PcCh_Alimento pcAlimento){
+  this.pcAlimento=pcAlimento;  
+}
 public String getEstado() {
     return chestado;
 }
